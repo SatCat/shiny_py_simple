60f51cb909e7f9225c424bb000aa8cad86252bc4
SatCat <39083134+SatCat@users.noreply.github.com>
2025-02-15 17:16:41 +1100

    port 80

--- app.py
+++ app.py
@@ -89,4 +89,4 @@ def server(input: Inputs, output: Outputs, session: Session):
 
 if __name__ == "__main__":
     app = App(app_ui, server, debug=True)
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=80)
